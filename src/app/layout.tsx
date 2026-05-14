c2db7035f4ffee7d574ded94db904a0d58b4334e
Sriyansh Gupta <sriyanshgupta24@gmail.com>
2026-05-14 05:06:15 +0000

    Get Started
    To start counting visitors and page views, follow these ste

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter, Syne } from 'next/font/google';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ 
   subsets: ['latin'], 
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body className={`${inter.variable} ${syne.variable} font-body antialiased bg-black text-white`}>
         {children}
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
